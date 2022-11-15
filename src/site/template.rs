3b6a7f74476681feeba4c8181900949d2772b88e
locusst <33630760+locusst@users.noreply.github.com>
2022-11-15 00:06:26 -0600

    fix issue with templating of style.css

--- src/site/template.rs
+++ src/site/template.rs
@@ -17,7 +17,7 @@ impl Template {
                 title {(title)}
                 meta charset="utf-8";
                 meta name="viewport" content="width=device-width, initial-scale=1.0";
-                link rel="stylesheet" href="style.css";
+                link rel="stylesheet" href="/style.css";
             }
         }
     }
